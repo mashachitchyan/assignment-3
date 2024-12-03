5fe534741f338b28fb3efd12b90fbf3e6115d584
Margarita Chitchyan <margarita.chitchyan@gmail.com>
2024-12-03 01:05:13 +0000

    HomeMain content updated

--- assignment-3/app/components/BenefitCard.tsx
+++ assignment-3/app/components/BenefitCard.tsx
@@ -2,15 +2,21 @@ import React from 'react'
 
 interface BenefitCardProps {
     heading: string;
-    number: number;
+    number: string;
     description: string;
+    variant?: 'default' | 'vertical';
 }
 
-const BenefitCard = ({ heading, number, description }: BenefitCardProps) => {
+const BenefitCard = ({ heading, number, description, variant }: BenefitCardProps) => {
+
+    const baseClasses = 'flex gap-4';
+    const verticalClasses = 'flex flex-col gap-1';
+    const defaultClasses = 'flex items-center';
+
     return (
         <div className='bg-off-black rounded-3xl p-4 border-dark-grey border flex flex-col justify-between text-left p-6'>
             <h4 className='text-lg bold-font pb-4'>{heading}</h4>
-            <div className='flex gap-4 items-center'>
+            <div className={`${baseClasses} ${variant === 'vertical' ? verticalClasses : defaultClasses}`}>
                 <p className='text-3xl text-lime bold-font'>{number}</p>
                 <p className='text-light-grey bold-font'>{description}</p>
             </div>
--- assignment-3/app/components/HomeMain.tsx
+++ assignment-3/app/components/HomeMain.tsx
@@ -8,14 +8,19 @@ function HomeMain() {
     <div>
       <Hero title='Mission' heroText='Turn insights into impact.' paragraph='Leverage real time analytics and detailed reporting to make smarter business decisions.' />
       <div id='benefits' className='flex-col text-center w-screen py-16 px-28'>
-      <p className='text-s text-lime uppercase bold-font'>What are the benefits?</p>
-      <p className='text-2xl'>All essential tools under one roof</p>
-      <div className='grid grid-cols-4 gap-4 h-[24vh] pt-4'>
-      <BenefitCard heading='Track ongoing orders' number={12} description='processing' />
-      <BenefitCard heading='Track ongoing orders' number={12} description='processing' />
-      <BenefitCard heading='Track ongoing orders' number={12} description='processing' />
-      <BenefitCard heading='Track ongoing orders' number={12} description='processing' />
+        <p className='text-s text-lime uppercase bold-font'>What are the benefits?</p>
+        <p className='text-2xl'>All essential tools under one roof</p>
+        <div className='grid grid-cols-4 gap-4 h-[30vh] pt-4'>
+          <BenefitCard heading='Track ongoing orders' number='12' description='processing' />
+          <BenefitCard heading='Customer insights' number='34' description='new buyers' />
+          <BenefitCard heading='Dynamic profit updates' number='$8524,17' description='$1,587.64 more than the month before' variant='vertical' />
+          <BenefitCard heading='Set sales goals' number='$2,300' description='this is 21% higher than this time last month' variant='vertical' />
+        </div>
       </div>
+      <div className='py-16 text-center w-screen'>
+        <p className='text-2xl'>Easy integration with various platforms:</p>
+        <div className='grid'>
+        </div>
       </div>
     </div>
   )
