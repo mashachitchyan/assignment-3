12a91ec646235f25b931ec0edf40957f5eb73c9c
Margarita Chitchyan <margarita.chitchyan@gmail.com>
2024-12-03 07:55:43 +0000

    Initial design for pricing cards added

--- assignment-3/app/pricing/page.tsx
+++ assignment-3/app/pricing/page.tsx
@@ -1,10 +1,11 @@
 import Header from "../components/Header";
+import PricingCard from "../components/PricingCard";
 
 export default function Home() {
   return (
     <div>
       <Header />
-
+      <PricingCard />
     </div>
   );
 }
