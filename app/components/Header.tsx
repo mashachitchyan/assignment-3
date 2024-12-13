637af3a9365b5b682a6f86d11d3e15d34682ef00
Margarita Chitchyan <margarita.chitchyan@gmail.com>
2024-12-13 15:45:27 +0000

    Header background fix attempt 3

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
   };
 
   return (
-    <header className="bg-white border-dark-grey bg-black">
+    <header className="bg-white border-dark-grey header">
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
         <Link href="/">
           <Image
